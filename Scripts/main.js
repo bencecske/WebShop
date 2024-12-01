e7561eb9fd1e62c1ab55af6b100722bdcfc03fd5
bencecske <62598262+bencecske@users.noreply.github.com>
2024-12-01 22:01:40 +0100

    Add files via upload

---
+++ Scripts/main.js
@@ -0,0 +1,11 @@
+function loginClick() {
+    window.location.replace("/login.html");
+}
+
+function cartClick() {
+    window.location.replace("/cart.html");
+}
+
+function menuClick() {
+    document.getElementById('menuBar').classList.toggle('show');
+}
