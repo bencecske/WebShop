27965922d1eb30e48defc32961913bae06f3afb5
bencecske <62598262+bencecske@users.noreply.github.com>
2024-12-01 22:12:08 +0100

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,9 +1,9 @@
 function loginClick() {
-    window.location.replace("WebShop/login.html");
+    window.location.replace("bencecske.github.io/WebShop/login.html");
 }
 
 function cartClick() {
-    window.location.replace("WebShop/cart.html");
+    window.location.replace("bencecske.github.io/WebShop/cart.html");
 }
 
 function menuClick() {
