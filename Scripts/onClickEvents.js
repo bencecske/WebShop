57002279c861a8b6d791a364943178ca36e35f7e
bencecske <62598262+bencecske@users.noreply.github.com>
2025-02-12 14:13:09 +0100

    Update onClickEvents.js

--- Scripts/onClickEvents.js
+++ Scripts/onClickEvents.js
@@ -9,13 +9,11 @@ function toCartClick() {
 }
 
 function loginClick() {
-    document.getElementById("login").style.display = "flex";
-    document.getElementById("registration").style.display = "none";
+    document.getElementById('login').classList.toggle('show');
 }
 
 function registerClick() {
-    document.getElementById("login").style.display = "none";
-    document.getElementById("registration").style.display = "flex";
+    document.getElementById('registration').classList.toggle('show');
 }
 
 function closeForm() {
