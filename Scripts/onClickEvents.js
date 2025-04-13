11bf3c47ff963c324bb8ce3438a33e286549561d
bencecske <62598262+bencecske@users.noreply.github.com>
2025-04-13 19:57:24 +0200

    Update onClickEvents.js

--- Scripts/onClickEvents.js
+++ Scripts/onClickEvents.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://diseases-green-neither-e.trycloudflare.com/';
+const apiUrl = 'https://arab-decor-julie-personal.trycloudflare.com/';
 
 async function loginClick() {
     let user = window.localStorage.getItem('username') || window.sessionStorage.getItem('username');
